59908f9cc7fd634e7bdb47b3e99298f14f9ab419
EXADEL\agalas <xcxcxc@dfdfv>
2018-08-01 14:20:25 +0300

    p1 - intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
-@NgModule({
+@NgModule({ // декоратор модуля. принимает на вход данные, необходимые для того, чтоб превратить класс AppModule в полнойенный NgModule
   declarations: [
-    AppComponent
+    AppComponent // здесь находится массив компонентов, которые относятся к данному модулю.
   ],
   imports: [
-    BrowserModule
+    BrowserModule, // здесь  представлены прочие модули, которые использует приложение ангулар
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // провайдеры обеспечивают обмен данными. Здесь находятся сервисы и др вспомогательные функции для манипуляций с данными
+  bootstrap: [AppComponent] // тут корневой компонент - с него начинается запуск приложения
 })
 export class AppModule { }
