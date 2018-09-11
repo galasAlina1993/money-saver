7ec8e4abfd72d856a7f0f9f1d18b30c415769522
unknown <ysukhov@exadel.com>
2018-09-11 17:12:37 +0300

    add post method

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
   public getToken(): string {
     return localStorage.getItem('token');
@@ -13,5 +15,10 @@ export class AuthService {
 
   public setToken(token): void {
     return localStorage.setItem('token', token);
+  }
+
+  public regiterUser(form) {
+    return this.http
+      .post<{login: string, password: string, token: string, name: string}>('http://localhost:3000/users', form);
   }
 }
